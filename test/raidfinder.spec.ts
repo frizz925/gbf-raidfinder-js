40d576e2b551d8b2643a8a0a8c877c783e25a0c7
Izra Faturrahman <Frizz925@hotmail.com>
2018-02-05 12:36:18 +0700

    Improved test case a bit

--- test/raidfinder.spec.ts
+++ test/raidfinder.spec.ts
@@ -6,12 +6,10 @@ describe('Test raidfinder', () => {
   dotenv.config();
 
   it('test tweet streaming', (cb) => {
-    new Raidfinder({
-      access_token_key: process.env.ACCESS_TOKEN,
-      access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-      consumer_key: process.env.CONSUMER_KEY,
-      consumer_secret: process.env.CONSUMER_SECRET
-    }).stream(celesteOmega.complete.en, (err, tweet, stream) => {
+    new Raidfinder().stream([
+      celesteOmega.complete.en,
+      celesteOmega.complete.jp
+     ], (err, tweet, stream) => {
       stream.destroy();
       cb();
     });
